5f72f0f8c479d7926cfb37dd127eb964d4bd7b13
qmei1123 <qmei@bu.edu>
2018-01-22 18:24:17 -0500

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var mouseX;
+var mouseY;
+var circleX = 300;
+var circleY = 300;
+var col = {
+    r: 0,
+    g: 0,
+    b: 0,
+    a: 0
+};
+function setup() {
+    createCanvas(1000, 900);
+}
+
+function draw() {  
+    circleX =  random(0, width);
+    circleY = random(0, height);
+    ellipse(mouseX, mouseY, 20, 20);
+    col.r =  map(mouseY-mouseX, 0, width, 200, 255);
+    col.g =  map(mouseX-mouseY, 0, height, 20, 255);
+    col.b =  map(mouseY*2, 0, width, 100, 150);
+    fill(col.r, col.g, col.b, 150);
+    stroke(60,40,200,0);
+     
+}
